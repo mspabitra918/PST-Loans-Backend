c06c5c0e54f58d8f252f2f6dd1d09ed7b2e47d56
Pabitra Ghara <pabitraghara384@gmail.com>
2026-04-02 13:45:38 +0530

    Refactor : 20260401120000_change_monthly_net_to_string.js

---
+++ src/db/migrations/20260401120000_change_monthly_net_to_string.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('leads', (table) => {
+    table.string('monthly_net').alter();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('leads', (table) => {
+    table.decimal('monthly_net').notNullable().alter();
+  });
+};
